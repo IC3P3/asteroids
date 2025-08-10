4f54b8041d77cfd18b1e49db7617c59a338daaae
Niklas Eifler <niklas.eifler@ptb.de>
2025-08-10 13:46:43 +0200

    Limit frame draw to 60 per second

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    timer = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +18,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = timer.tick(60) / 1000
 
 
 if __name__ == "__main__":
