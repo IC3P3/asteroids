a428174b0fcf83b626586ac58e0da26e5bc5f42d
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2025-08-11 12:06:13 +0200

    Added asteroid spawning

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 from constants import (SCREEN_WIDTH, SCREEN_HEIGHT)
 from player import Player
-from astroid import Astroid
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -18,9 +19,11 @@ def main():
     astroids = pygame.sprite.Group()
 
     Player.containers = (updateable, drawable)
-    Astroid.containers = (astroids, updateable, drawable)
+    Asteroid.containers = (astroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
